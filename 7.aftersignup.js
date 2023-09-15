1acbf66f9edc1ec878cd021e0d8d146281e13276
Fay Ivana Karin <145007482+fykarins@users.noreply.github.com>
2023-09-16 00:25:36 +0700

    Kode JAVASCRIPT Menu "After Sign Up"

---
+++ 7.aftersignup.js
@@ -0,0 +1,12 @@
+const nama="smartnametag";
+let usia=30;
+
+const biodata = document.getElementById('biodata');
+console.log(biodata);
+
+function generateBiodata
+
+console.log(`nama saya adalah ${nama} dan usia saya ${usia} tahun`)
+
+
+
